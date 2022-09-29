fc723341d40f172cc83fa753f2374aee8be780dc
joaquimserafim <joaquimserafim@users.noreply.github.com>
2022-09-29 18:15:29 +0100

    v2.3.1

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export * from "./guard/index";
 export * from "./log/index";
 export * from "./thrower/index";
 export * as aws from "./aws/index";
-export * as multipartParser from "./multipart-parser/index";
+export * from "./multipart-parser/index";
